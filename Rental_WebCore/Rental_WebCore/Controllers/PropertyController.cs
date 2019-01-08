801cf7846c87ddf8d70d62afff1edca580bf5158
Tam <darkthanhtam@gmail.com>
2019-01-08 23:13:40 +0700

    input-rental

---
+++ Rental_WebCore/Rental_WebCore/Controllers/PropertyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rental_WebCore.Controllers
+{
+    public class PropertyController : Controller
+    {
+        public IActionResult Property(int id)
+        {
+            return View((object)id);
+        }
+
+        public IActionResult ListProperty(int id)
+        {
+            return View((object)id);
+        }
+    }
+}
