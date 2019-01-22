9a46a778c15e824581ad31b02b300701cc78500d
Tam <darkthanhtam@gmail.com>
2019-01-22 14:59:37 +0700

    new_project

---
+++ Rental_WebCore/Rental_WebCore/Controllers/MaintenanceController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rental_WebCore.Controllers
+{
+    public class MaintenanceController : Controller
+    {
+        public IActionResult DashBoarchQLBTT()
+        {
+            return View();
+        }
+        public IActionResult Detailtask()
+        {
+            return View();
+        }
+        public IActionResult Maintenance()
+        {
+            return View();
+        }
+        public IActionResult Task()
+        {
+            return View();
+        }
+        
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+        public IActionResult EditTask()
+        {
+            return View();
+        }
+       
+        
+        public IActionResult AddMaintenance()
+        {
+            return View();
+        }
+        public IActionResult AddMaintenance1()
+        {
+            return View();
+        }
+        public IActionResult EditMaintenance()
+        {
+            return View();
+        }
+        public IActionResult DetailMaintenance()
+        {
+            return View();
+        }
+        public IActionResult Text()
+        {
+            return View();
+        }
+
+    }
+}
--- Rental_WebCore/Rental_WebCore/Controllers/PropertyController.cs
+++ Rental_WebCore/Rental_WebCore/Controllers/PropertyController.cs
@@ -8,14 +8,39 @@ namespace Rental_WebCore.Controllers
 {
     public class PropertyController : Controller
     {
-        public IActionResult Property(int id)
+        public IActionResult Index(string id)
         {
             return View((object)id);
         }
 
-        public IActionResult ListProperty(int id)
+        public IActionResult AddNew(int id)
         {
             return View((object)id);
+        }
+
+        public IActionResult Create()
+        {
+            return RedirectToAction("ListProperty");
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Save()
+        {
+            return RedirectToAction("ListProperty");
+        }
+
+        public IActionResult Delete()
+        {
+            return RedirectToAction("ListProperty");
+        }
+
+        public IActionResult ListProperty()
+        {
+            return View();
         }
     }
 }
