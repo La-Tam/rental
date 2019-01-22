a925c4b8a1819a5ba39ebb541bbe8a3f2e656a99
phuoclv <levanphuoc2809@gmail.com>
2019-01-22 15:13:36 +0700

    FIRST COMMIT

---
+++ Rental_WebCore/Rental_WebCore/Controllers/AssetController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Rental_WebCore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rental_WebCore.Controllers
+{
+    public class AssetController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult HomeAsset()
+        {
+            return View();
+        }
+        public IActionResult TypesAsset()
+        {
+            return View();
+        }
+        public IActionResult ViewDetailAsset()
+        {
+            return View();
+        }
+  
+    }
+}
